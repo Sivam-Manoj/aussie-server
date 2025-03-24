9137291a20f65aa1c571a85913cce3a7f0ef410b
Harshika Maheswaran <IT22064554@my.sliit.lk>
2025-03-24 16:26:22 +0530

    updatedeleteplayer

--- src/routes/playerRoutes/playerRoutes.ts
+++ src/routes/playerRoutes/playerRoutes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import { createPlayerController } from '../../controller/playerControllers/createPlayerController.js';
 import { getPlayerController } from '../../controller/playerControllers/getPlayerController.js';
+import { deletePlayerController } from '../../controller/playerControllers/deletePlayerController.js';
+import { updatePlayerController } from '../../controller/playerControllers/updatePlayerController.js';
 
 const router = Router();
 
 router.post('/create', createPlayerController);
 router.post('/get', getPlayerController);
+router.post('/delete',deletePlayerController);
+router.post('/update',updatePlayerController)
 export default router;
--- src/routes/userRoutes/userRoutes.ts
+++ src/routes/userRoutes/userRoutes.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express';
-
+import { authMiddleware } from '../../middleware/auth/authMiddleware.js';
+import { signupController } from '../../controller/userControllers/signup.controller.js';
+import { signinController } from '../../controller/userControllers/signin.controller.js';
+import { logoutController } from '../../controller/userControllers/logout.controller.js';
 const router = Router();
 
-router.post('/signup');
+// Public routes
+router.post('/register', signupController);  // Signup route
+router.post('/login', signinController);     // Signin route
+router.post('/logout', authMiddleware, logoutController);  // Logout route (protected)
 
 export default router;
